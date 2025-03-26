e83d6175fe92cd5e2742df7e9dc426b96349925a
Ravi Ranjan <193577335+ravrranj@users.noreply.github.com>
2025-03-27 01:17:12 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,10 @@
+
+package bank;  //Declares that this class belongs to 'bank' package
+
+public class Account {
+    public String name;
+    
+public void display() {
+    System.out.println("Account Holder: " + name);
+}
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+
+import bank.Account;  // Import the Account class from bank package
+
+public class Main{
+    public static void main(String[] args) {
+        Account acc = new Account();
+        acc.name ="Ravi";
+        acc.display();
+    }
+}
